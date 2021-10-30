689c73bb25763370c2d24124677fe8c9ca177467
Avast_pro <38318102+hobot2018@users.noreply.github.com>
2021-10-30 16:36:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -143,8 +143,28 @@ Flight::route('/api/dishes/cat/@cat', function($cat){
     }   
     echo json_encode($out);
 });
+Flight::route('/api/order/@id', function($id){
+    $servername = "localhost";
+    $database = "api";
+    $username = "based";
+    $password = "1234";
+    // Создаем соединение
+    $error = "0";
 
-Flight::route('/hats/@id', function($id){
-    echo 'hello world!'.$id;
+    $res = 12;
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    $sql = "SELECT * FROM `orders` WHERE `id`=".$id;
+    $q = mysqli_query($conn, $sql);
+    $id = 1;
+    while ($row = mysqli_fetch_assoc($q)) {
+                                            
+                                                
+        $out[$id][] = $row;
+        $id = $id +1;
+                                                
+                                                
+    }   
+    echo json_encode($out);
 });
+
 Flight::start();
